b6f1368f709fb0debf5e3df1165103fff4d68aa5
bokwoon <bokwoon.c@gmail.com>
2023-07-11 00:42:46 +0800

    checkpoint

--- notebrew_test.go
+++ notebrew_test.go
@@ -1,9 +1,17 @@
 package nb5
 
 import (
+	"bytes"
+	"errors"
+	"fmt"
 	"io"
+	"io/fs"
 	"net/http"
+	"path"
+	"strings"
 	"testing"
+	"testing/fstest"
+	"time"
 
 	"github.com/bokwoon95/nb5/internal/testutil"
 )
@@ -113,8 +121,6 @@ func Test_validatePath(t *testing.T) {
 	}
 }
 
-// TODO: a mock file system that we can use in tests
-
 func Test_create(t *testing.T) {
 	type TestTable struct {
 		description    string
@@ -126,4 +132,135 @@ func Test_create(t *testing.T) {
 		wantStatusCode int       // response status code
 		wantLocation   string    // response Location header (without the raw query after the "?")
 	}
+}
+
+type TestFS struct {
+	fstest.MapFS
+}
+
+func (fsys TestFS) OpenWriter(name string) (io.WriteCloser, error) {
+	if !fs.ValidPath(name) {
+		return nil, &fs.PathError{Op: "openwriter", Path: name, Err: fs.ErrInvalid}
+	}
+	testFile := &TestFile{
+		mapFS: fsys.MapFS,
+		name:  name,
+		buf:   &bytes.Buffer{},
+	}
+	return testFile, nil
+}
+
+func (fsys TestFS) MkdirAll(path string, perm fs.FileMode) error {
+	if !fs.ValidPath(path) {
+		return &fs.PathError{Op: "mkdirall", Path: path, Err: fs.ErrInvalid}
+	}
+	fsys.MapFS[path] = &fstest.MapFile{
+		Mode:    fs.ModeDir,
+		ModTime: time.Now(),
+	}
+	return nil
+}
+
+func (fsys TestFS) RemoveAll(path string) error {
+	if !fs.ValidPath(path) {
+		return &fs.PathError{Op: "removeall", Path: path, Err: fs.ErrInvalid}
+	}
+	delete(fsys.MapFS, path)
+	pathPrefix := path + "/"
+	for name := range fsys.MapFS {
+		if strings.HasPrefix(name, pathPrefix) {
+			delete(fsys.MapFS, name)
+		}
+	}
+	return nil
+}
+
+func (fsys TestFS) Move(oldpath, newpath string) error {
+	if !fs.ValidPath(oldpath) {
+		return &fs.PathError{Op: "move", Path: oldpath, Err: fs.ErrInvalid}
+	}
+	if !fs.ValidPath(newpath) {
+		return &fs.PathError{Op: "move", Path: newpath, Err: fs.ErrInvalid}
+	}
+	oldFileInfo, err := fs.Stat(fsys.MapFS, oldpath)
+	if err != nil {
+		// If source file/directory does not exist, no point in moving
+		// anything.
+		return err
+	}
+	var data []byte
+	if !oldFileInfo.IsDir() {
+		data, err = fs.ReadFile(fsys.MapFS, oldpath)
+		if err != nil {
+			return err
+		}
+	}
+	newFileInfo, err := fs.Stat(fsys.MapFS, newpath)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			// If destination does not exist, the file or directory can safely
+			// take its place. Copy the data and filemode over.
+			delete(fsys.MapFS, oldpath)
+			fsys.MapFS[newpath] = &fstest.MapFile{
+				Data:    data,
+				Mode:    oldFileInfo.Mode(),
+				ModTime: time.Now(),
+			}
+			return nil
+		}
+		return err
+	}
+	if oldFileInfo.IsDir() {
+		// A directory cannot be moved if its destination exists, no matter if
+		// the destination is a file or another directory.
+		return fmt.Errorf("%s already exists", newpath)
+	}
+	if newFileInfo.IsDir() {
+		// Move file into directory.
+		delete(fsys.MapFS, oldpath)
+		fsys.MapFS[path.Join(newpath, oldFileInfo.Name())] = &fstest.MapFile{
+			Data:    data,
+			Mode:    oldFileInfo.Mode(),
+			ModTime: time.Now(),
+		}
+		return nil
+	}
+	fsys.MapFS[newpath] = &fstest.MapFile{
+		Data:    data,
+		Mode:    oldFileInfo.Mode(),
+		ModTime: time.Now(),
+	}
+	return nil
+}
+
+type TestFile struct {
+	mapFS       fstest.MapFS
+	name        string
+	buf         *bytes.Buffer
+	writeFailed bool
+}
+
+func (f *TestFile) Write(p []byte) (n int, err error) {
+	n, err = f.buf.Write(p)
+	if err != nil {
+		f.writeFailed = true
+	}
+	return n, err
+}
+
+func (f *TestFile) Close() error {
+	if f.buf == nil {
+		return fmt.Errorf("already closed")
+	}
+	defer func() {
+		f.buf = nil
+	}()
+	if f.writeFailed {
+		return nil
+	}
+	f.mapFS[f.name] = &fstest.MapFile{
+		Data:    f.buf.Bytes(),
+		ModTime: time.Now(),
+	}
+	return nil
 }
